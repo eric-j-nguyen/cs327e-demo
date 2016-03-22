8d961258f6441f610bc7888d56a582d91162edd6
Eric J Nguyen <ericweb2@gmail.com>
2016-03-21 19:35:21 -0500

    Uncomment the rest of the code

--- python-demo.py
+++ python-demo.py
@@ -1,98 +1,98 @@
 print("Hello World!")
 
 
-# print()
-# print("Begin Variables")
-# x = 1
-# y = 2
-# print("x=", x)
-# print("y=", y)
+print()
+print("Begin Variables")
+x = 1
+y = 2
+print("x=", x)
+print("y=", y)
 
 
-# print()
-# print("Begin Operators")
+print()
+print("Begin Operators")
 
-# print(x+y)
-# z = x + y
-# print("z=", z)
-# print(z < y)
-# print(z != y)
-
-
-# print()
-# print("Begin User Input")
-
-# nameStr = input("Name: ")
-# age = eval(input("Age: "))
-# print ("Your name is", nameStr, "and you are", age)
-
-# print()
-# print("Begin Functions")
-
-# def seven():
-#   return 7
-
-# def addNums(num1, num2):
-#   return num1+num2
-
-# print(addNums(7,8))
-# print(seven())
-
-# print()
-# print("Begin Loops")
-
-# for i in range(0,10):
-#   print(i)
+print(x+y)
+z = x + y
+print("z=", z)
+print(z < y)
+print(z != y)
 
 
-# print("Begin Loops")
+print()
+print("Begin User Input")
 
-# for i in range(0,10):
-#   if (i % 2 == 1):
-#     print("Found an odd. Breaking")
-#     break
-#   else:
-#     print(i)
+nameStr = input("Name: ")
+age = eval(input("Age: "))
+print ("Your name is", nameStr, "and you are", age)
 
+print()
+print("Begin Functions")
 
-# for i in range(0,10):
-#   if (i % 2 == 1):
-#     print("Found an odd. Skipping.")
-#     continue
-#   else:
-#     print(i)
+def seven():
+  return 7
 
+def addNums(num1, num2):
+  return num1+num2
 
-# print()
-# print("Begin Strings")
+print(addNums(7,8))
+print(seven())
 
-# string = "Hello World!"
-# print(string)
-# print(string[1])
-# print(string[1:])
-# print(string[:-2])
+print()
+print("Begin Loops")
+
+for i in range(0,10):
+  print(i)
 
 
-# print()
-# print("Begin File Input")
+print("Begin Loops")
 
-# inFile = open("inputFile.txt", "r")
+for i in range(0,10):
+  if (i % 2 == 1):
+    print("Found an odd. Breaking")
+    break
+  else:
+    print(i)
 
-# for line in inFile:
-#   print(line.rstrip("\n"))
 
-# inFile.close()
+for i in range(0,10):
+  if (i % 2 == 1):
+    print("Found an odd. Skipping.")
+    continue
+  else:
+    print(i)
 
-# print()
-# print("Begin File Output")
 
-# outFile = open("outputFile.txt", "w")
-# outFile.write("Hello World!\n")
-# outFile.close()
+print()
+print("Begin Strings")
 
-# outFile = open("outputFile.txt", "w")
-# outFile.write("Hello World 2!\n")
-# outFile.close()
+string = "Hello World!"
+print(string)
+print(string[1])
+print(string[1:])
+print(string[:-2])
+
+
+print()
+print("Begin File Input")
+
+inFile = open("inputFile.txt", "r")
+
+for line in inFile:
+  print(line.rstrip("\n"))
+
+inFile.close()
+
+print()
+print("Begin File Output")
+
+outFile = open("outputFile.txt", "w")
+outFile.write("Hello World!\n")
+outFile.close()
+
+outFile = open("outputFile.txt", "w")
+outFile.write("Hello World 2!\n")
+outFile.close()
 
 
 print()
